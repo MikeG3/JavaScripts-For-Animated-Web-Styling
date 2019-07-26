dceb940e6178985c6f484283beb273ddc8af120e
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-26 10:40:05 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,6 +1,20 @@
+//canvas can now be called by this variable
 var canvas = document.querySelector('canvas');
 
+//set canvas height and width
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
 
+//Gives canvas API 2d drawing functions
+var c.canvas.getContext('2d');
+
+c.fillRect( 25, 25, 25, 25);
+
+
+
+
+
+//output to console
 console.log(canvas);
