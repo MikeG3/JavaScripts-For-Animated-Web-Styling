37bfc31eae72cc3946981d742a48f1b456c8626a
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-26 17:00:59 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -13,7 +13,7 @@ var c = canvas.getContext('2d');
 
 //strat drawing
 
-c.fillStyle = "#2222DE";
+//c.fillStyle = "#2222DE";
 c.fillRect( 100, 100, 50, 50);
 c.fillRect( 0, 0, 10, 100);
 c.fillRect( 10, 10, 15, 25);
