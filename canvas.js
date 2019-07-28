286cbb241eeb64e390d89adf416761f94f246f0a
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-28 00:29:22 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -62,7 +62,7 @@ function animate() {
   if (squareSize < 5) {
     incrementing = true;
   }
-    if (squareSize > (windowHeight/2) ||squareSize > (windowWidth/2)) {
+    if (squareSize > 100 || squareSize > (windowHeight/2) || squareSize > (windowWidth/2)) {
     incrementing = false;
   }
   if (incrementing){
