b658843168cf1b961b5b75da0b68697e515066d8
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-27 23:59:04 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -68,7 +68,7 @@ for (i = 0 ; i < windowHeight ; i+=squareSize ){
     aSquareG %= 255;
     aSquareB += 15;
     aSquareB %= 255;
-   c.fillStyle = 'rgba(255, 165, 0, 1)';
+   c.fillStyle = 'rgba(aSquareR, 165, 0, 1)';
     // c.fillStyle = 'rgba(aSquareR, aSquareG, aSquareB, 0.5 )';
     c.fillRect(j, i, 40, 40);  
 //     document.write("aSquareR = " +aSquareR + " <br>  ");
