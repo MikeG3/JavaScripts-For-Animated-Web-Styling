508df96598c617541da785a11f62d00c77ca90e4
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-27 22:57:55 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -43,7 +43,31 @@ document.write("Window height = " + windowHeight + "<br>");
 
 function animate() {
   
+  //variables 
+var i = 0;
+var j = 0;
+var squareSize = 40;
+var aSquareR = 0;
+var aSquareG = 0;
+var aSquareB = 0;
+var bSquareR = 0;
+var bSquareG = 0;
+var bSquareB = 0;
   
+  //CHECKERED BOARD
+for (i = 0 ; i < windowHeight ; i+=squareSize ){
+  for (j = 0 ; j < windowWidth ; j+=(2*squareSize) ) {  //j = (2*squareSize)
+    if ( (j==0) && (i/40%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
+      j += squareSize;
+    }//close if
+    c.fillRect(j, i, 40, 40);  
+    document.write("i = " +i + "   ");
+    document.write("j= " + j + "<br>");
+  }//close inner for loop
+  //update colors
+}//close outter for loop
+document.write("Window width = " + windowWidth + "<br>");
+document.write("Window height = " + windowHeight + "<br>");
   
   
 requestAnimationFrame(animate);
