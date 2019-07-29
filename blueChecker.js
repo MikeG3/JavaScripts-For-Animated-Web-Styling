fe5a48ff94c7131f1a78d61e9b38285f8a6e763b
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-29 14:11:26 -0400

    Update blueChecker.js

--- blueChecker.js
+++ blueChecker.js
@@ -50,8 +50,6 @@ function animate() {
   //CHECKERED BOARD
 for (i = 0 ; i < windowHeight ; i+=squareSize ){
   for (j = 0 ; j < windowWidth ; j+=(squareSize) ) {  //j = (2*squareSize)
-    if ( (j==0) && (i/squareSize%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
-      j += squareSize;
     }//close if
     //update colors
     aSquareR += 5;
