f204a64f155b1311ed3067d957e1583635714b4f
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-29 17:46:46 -0400

    Update moveInGrid.js

--- moveInGrid.js
+++ moveInGrid.js
@@ -10,10 +10,12 @@ var windowHeight = window.innerHeight;
 //Gives canvas API 2d drawing functions
 var c = canvas.getContext('2d');
 
-//variables
+//GLOBAL VARIABLES
 var frameCounter = 0;
 var i = 0;
 var j = 0;
+var xPos = 0;
+var yPos = 0;
 var squareSize = 50;
 var aSquareR = 0;
 var aSquareG = 0;
@@ -31,80 +33,34 @@ var incrementing = true;
 
 c.fillStyle = "#FF00EE";
 
-//CHECKERED BOARD
-// for (i = 0 ; i < windowHeight ; i+=squareSize ){
-//   for (j = 0 ; j < windowWidth ; j+=(2*squareSize) ) {  //j = (2*squareSize)
-//     if ( (j==0) && (i/40%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
-//       j += squareSize;
-//     }//close if
-//     c.fillRect(j, i, 40, 40);  
-//     document.write("i = " +i + "   ");
-//     document.write("j= " + j + "<br>");
-//   }//close inner for loop
-//   //update colors
-// }//close outter for loop
-// document.write("Window width = " + windowWidth + "<br>");
-// document.write("Window height = " + windowHeight + "<br>");
 
 function animate() {
   
-  //variables 
+  //LOCAL VARIABLES 
 
   
-  //UPDATE SQUARE SIZE
-  if (squareSize < 5) {
-    incrementing = true;
-  }
-    if (squareSize > 100 || squareSize > (windowHeight/2) || squareSize > (windowWidth/2)) {
-    incrementing = false;
-  }
-  if (incrementing){
-    squareSize++;
-  }
-    else {
-      squareSize--;
-    }
-  
   //CHECKERED BOARD
-for (i = 0 ; i < windowHeight ; i+=squareSize ){
-  for (j = 0 ; j < windowWidth ; j+=(2*squareSize) ) {  //j = (2*squareSize)
-    if ( (j==0) && (i/squareSize%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
-      j += squareSize;
+for (i = 0 ; i < 10 ; i+=squareSize ){
+  for (j = 0 ; j < 10 ; j+=squareSize ) {  //j = (2*squareSize)
+   
+    //UPDATE COLORS
+    
+    //CREATE CHECKER BOARD
+    if ( i%2 == 0 && j%2 == 0){
+      c.fillStyle = 'rgba(50, 50, 50, 0.5)';
     }//close if
-    //update colors
-    aSquareR += 5;
-    aSquareR %= 255;
-    aSquareG += 25;
-    aSquareG %= 255;
-    aSquareB += 15;
-    aSquareB %= 255;
-    c.fillStyle = 'rgba(0,100,' + aSquareB + ','+ aSquareA + ')';
+    else if ( i%2 == 1 && j%2 = 1 ){
+      c.fillStyle = 'rgba(250, 250, 250, 0.5)';
+    }
     c.fillRect(j, i, squareSize, squareSize);  
   }//close inner for loop
   
-  //UPDATE COLORS
-  aSquareR += 5;
-  aSquareR %= 255;
-  aSquareG += 25;
-  aSquareG %= 255;
-  aSquareB += 15;
-  aSquareB %= 255;
-  c.fillStyle = 'rgba(' + aSquareR + ',' + aSquareG + ',' + aSquareB + ','+ aSquareA + ')';
+  //DRAW CURRENT POSITION SQUARE
   
 }//close outter for loop
-// document.write("Window width = " + windowWidth + "<br>");
-// document.write("Window height = " + windowHeight + "<br>");
-  
+ 
 //TEXT
-c.font = "30px Comic Sans MS";
-grayColor = frameCounter % 255;
-c.fillStyle = "rgb(" + grayColor + "," + grayColor + "," + grayColor + ")";
 
-
-c.textAlign = "center";
-c.fillText("Michael Garner Designs", canvas.width/2, canvas.height/2);   
-  
-  
 //ANIMATE
 frameCounter++;
 requestAnimationFrame(animate);
