acbd6db33c2659599c5687bd238ba0c3ae9858a6
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-30 19:36:49 -0400

    Update moveInGrid.js

--- moveInGrid.js
+++ moveInGrid.js
@@ -38,8 +38,8 @@ window.addEventListener('keydown', move );
 function move(key) {
     if (key.keyCode == 37) {xPos = (xPos-1)%gridSizeX; if (xPos < 0) { xPos += gridSizeX; } }
     if (key.keyCode == 39) {xPos = (xPos+1)%gridSizeX; if (xPos > gridSizeX) { xPos -= gridSizeX; } }
-    if (key.keyCode == 38) {yPos = (yPos-1)%gridSizeY; if (yPos < 0) { yPos += gridSizeY; } }
-    if (key.keyCode == 40) {yPos = (yPos+1)%gridSizeY; if (yPos > gridSizeY) { yPos -= gridSizeY; } }
+    if (key.keyCode == 40) {yPos = (yPos-1)%gridSizeY; if (yPos < 0) { yPos += gridSizeY; } }
+    if (key.keyCode == 38) {yPos = (yPos+1)%gridSizeY; if (yPos > gridSizeY) { yPos -= gridSizeY; } }
 }//close move function
 
                             
