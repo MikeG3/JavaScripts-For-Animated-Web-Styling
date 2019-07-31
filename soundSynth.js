8df48be26749e039e9e394246999c5fa9a81f7ea
Mike <39208141+MikeG3@users.noreply.github.com>
2019-07-30 22:53:43 -0400

    Update soundSynth.js

--- soundSynth.js
+++ soundSynth.js
@@ -2,10 +2,10 @@
 var canvas = document.querySelector('canvas');
 
 //set canvas height and width
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+var windowWidth = window.innerWidth - 10;
+var windowHeight = window.innerHeight - 10;
 
 //Gives canvas API 2d drawing functions
 var c = canvas.getContext('2d');
